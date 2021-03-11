baaee4838592ab7b089cb6214c8f783de9833ebb
jdiph051 <32931172+jdiph051@users.noreply.github.com>
2021-03-11 12:13:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, Sello Node.js'));
-app.get('/', (req, res) => res.send(''));
-app.get('/', (req, res) => res.send('Version2'));
+app.get('/', (req, res) => res.send('Hello World!, Sello Node.js + Add Vers2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
