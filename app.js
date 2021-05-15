f55ca8676f5d600f997903c1107601f92ae619d8
jdiph051 <32931172+jdiph051@users.noreply.github.com>
2021-05-15 11:10:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Di_Data CI/CD Pipeline V1, V2, V3 TEST..!!..!!'));
+app.get('/', (req, res) => res.send('Hello Di_Data CI/CD Pipeline V1, V2, V3 TEST + Papzez..!!..!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
